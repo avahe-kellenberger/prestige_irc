1517a01db915edc952371da4559689729d7d6460
Avahe Kellenberger <avahe@protonmail.ch>
2018-11-24 18:55:29 -0500

    Updated .gitignore
    Changed setup.py to have a correct version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="prestige_irc",
-   version="0.1",
+   version="0.0.1",
    description="A simple API for IRC networks",
    author="Avahe Kellenberger",
    author_email="avahe@protonmail.ch",
