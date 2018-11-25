f0cf8c0608cd4d9e842968a0c41441707d347de3
Avahe Kellenberger <avahe@protonmail.ch>
2018-11-24 19:09:40 -0500

    Updated setup.py to include more information, and added the GNU GPL v2.0 license.

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-   name="prestige_irc",
-   version="0.0.1",
-   description="A simple API for IRC networks",
-   author="Avahe Kellenberger",
-   author_email="avahe@protonmail.ch",
-   packages=["prestige_irc"]
+    name="prestige_irc",
+    version="0.0.1",
+    author="Avahe Kellenberger",
+    author_email="avahe@protonmail.ch",
+    description="A simple API for IRC networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avahe-kellenberger/prestige_irc",
+    packages=["prestige_irc"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent"
+    ]
 )
