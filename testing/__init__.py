66d0386742bdd2107a31fd2b6fb85dceec1332af
Avahe Kellenbeger <avahe@protonmail.ch>
2019-06-06 19:17:02 -0400

    Removed testing folder and added to gitignore.

--- testing/__init__.py
+++
@@ -1,9 +0,0 @@
-from prestige_irc.irc_connection import IRCConnection
-
-if __name__ == '__main__':
-
-    rizon_bot = IRCConnection('PrestigeBot')
-    rizon_bot.connect('irc.rizon.net')
-
-    mopar_bot = IRCConnection('PrestigeBot')
-    mopar_bot.connect('irc.moparisthebest.com')
